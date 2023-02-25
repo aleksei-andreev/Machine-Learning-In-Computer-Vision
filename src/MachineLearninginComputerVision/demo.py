af2e733539eb7eb50ecb9193d4e876073635c65f
aleksei-andreev <gamer995@gmail.com>
2023-02-25 23:43:13 +0300

    Apply code formatter
    q
    git status

--- src/MachineLearninginComputerVision/demo.py
+++ src/MachineLearninginComputerVision/demo.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # coding=utf-8
-#================================================================
+# ================================================================
 #   Copyright (C) 2018 * Ltd. All rights reserved.
 #
 #   Editor      : VIM
@@ -9,7 +9,7 @@
 #   Created date: 2018-11-30 15:56:37
 #   Description :
 #
-#================================================================
+# ================================================================
 
 import cv2
 import time
@@ -23,17 +23,17 @@ from PIL import Image
 direct_path = osp.abspath(osp.dirname(__file__)).replace(os.sep, "/")
 
 return_elements = ["input/input_data:0", "pred_sbbox/concat_2:0", "pred_lbbox/concat_2:0"]
-pb_file         = direct_path + "/model/yolov3_nano_416.pb"
-video_path      = direct_path + "/sample/road.mp4"
+pb_file = direct_path + "/model/yolov3_nano_416.pb"
+video_path = direct_path + "/sample/road.mp4"
 # video_path      = 0
-num_classes     = 20
-input_size      = 416
-graph           = tf.Graph()
-return_tensors  = utils.read_pb_return_tensors(graph, pb_file, return_elements)
+num_classes = 20
+input_size = 416
+graph = tf.Graph()
+return_tensors = utils.read_pb_return_tensors(graph, pb_file, return_elements)
 
-print("input.name =", return_tensors[0].name)   # import/input/input_data:0
-print("output1.name =", return_tensors[1].name) # import/pred_sbbox/concat_2:0
-print("output2.name =", return_tensors[2].name) # import/pred_lbbox/concat_2:0
+print("input.name =", return_tensors[0].name)  # import/input/input_data:0
+print("output1.name =", return_tensors[1].name)  # import/pred_sbbox/concat_2:0
+print("output2.name =", return_tensors[2].name)  # import/pred_lbbox/concat_2:0
 
 
 with tf.compat.v1.Session(graph=graph) as sess:
@@ -51,26 +51,25 @@ with tf.compat.v1.Session(graph=graph) as sess:
         prev_time = time.time()
 
         pred_sbbox, pred_lbbox = sess.run(
-			[return_tensors[1], return_tensors[2]],
-					feed_dict={ return_tensors[0]: image_data})
+            [return_tensors[1], return_tensors[2]], feed_dict={return_tensors[0]: image_data}
+        )
 
-        pred_bbox = np.concatenate([np.reshape(pred_sbbox, (-1, 5 + num_classes)),
-                                    np.reshape(pred_lbbox, (-1, 5 + num_classes))], axis=0)
+        pred_bbox = np.concatenate(
+            [np.reshape(pred_sbbox, (-1, 5 + num_classes)), np.reshape(pred_lbbox, (-1, 5 + num_classes))],
+            axis=0,
+        )
 
         bboxes = utils.postprocess_boxes(pred_bbox, frame_size, input_size, 0.3)
-        bboxes = utils.nms(bboxes, 0.45, method='nms')
+        bboxes = utils.nms(bboxes, 0.45, method="nms")
         image = utils.draw_bbox(frame, bboxes)
 
         curr_time = time.time()
         exec_time = curr_time - prev_time
         result = np.asarray(image)
-        info = "time: %.2f ms" %(1000*exec_time)
+        info = "time: %.2f ms" % (1000 * exec_time)
         cv2.namedWindow("result", cv2.WINDOW_NORMAL)
         result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         result_s = cv2.resize(result, (1920, 1080))
         cv2.imshow("result", result_s)
-        if cv2.waitKey(1) & 0xFF == ord('q'): break
-
-
-
-
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
