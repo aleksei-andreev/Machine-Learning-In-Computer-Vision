78f6ffdd2b4b8adc9b8a86fc65c4654126ef244c
aleksei-andreev <gamer995@gmail.com>
2023-02-27 01:21:59 +0300

    Update files

---
+++ tests/test_demo.py
@@ -0,0 +1,14 @@
+import os
+import os.path as osp
+
+import cv2
+import numpy as np
+
+from MachineLearninginComputerVision import demo
+
+# direct_path = osp.abspath(osp.dirname(__file__)).replace(os.sep, "/")
+
+
+# def test_regression():
+#    true = demo.main(video_path=direct_path + "/sample/road.mp4", show=False)
+#    pred = demo.main(video_path=direct_path + "/sample/result.mp4", show=False)
