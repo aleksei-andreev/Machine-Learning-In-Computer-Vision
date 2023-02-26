f9d2eb447c01575d4d2f10ca94a1f958f172a4a6
aleksei-andreev <gamer995@gmail.com>
2023-02-27 02:56:34 +0300

    Update files

--- src/MachineLearninginComputerVision/demo.py
+++ src/MachineLearninginComputerVision/demo.py
@@ -16,20 +16,16 @@ import os.path as osp
 import cv2
 import numpy as np
 import tensorflow as tf
-from PIL import Image
 
 import MachineLearninginComputerVision.core.utils as utils
-
-# import time
-
 
 direct_path = osp.abspath(osp.dirname(__file__)).replace(os.sep, "/")
 
 
-def main(video_path=direct_path + "/sample/road.mp4", show=True):
+def main():
     return_elements = ["input/input_data:0", "pred_sbbox/concat_2:0", "pred_lbbox/concat_2:0"]
     pb_file = direct_path + "/model/yolov3_nano_416.pb"
-    # video_path      = 0
+    image_path = direct_path + "/sample/road.jpg"
     num_classes = 20
     input_size = 416
     graph = tf.Graph()
@@ -40,44 +36,33 @@ def main(video_path=direct_path + "/sample/road.mp4", show=True):
     print("output2.name =", return_tensors[2].name)  # import/pred_lbbox/concat_2:0
 
     with tf.compat.v1.Session(graph=graph) as sess:
-        vid = cv2.VideoCapture(video_path)
-        while True:
-            return_value, frame = vid.read()
-            if return_value:
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                image = Image.fromarray(frame)
-            else:
-                raise ValueError("No image!")
-            frame_size = frame.shape[:2]
-            image_data = utils.image_preporcess(np.copy(frame), [input_size, input_size])
-            image_data = image_data[np.newaxis, ...]
-            # prev_time = time.time()
+        frame = cv2.imread(image_path, cv2.COLOR_BGR2RGB)
+        frame_size = frame.shape[:2]
+        image_data = utils.image_preporcess(np.copy(frame), [input_size, input_size])
+        image_data = image_data[np.newaxis, ...]
 
-            pred_sbbox, pred_lbbox = sess.run(
-                [return_tensors[1], return_tensors[2]], feed_dict={return_tensors[0]: image_data}
-            )
+        pred_sbbox, pred_lbbox = sess.run(
+            [return_tensors[1], return_tensors[2]], feed_dict={return_tensors[0]: image_data}
+        )
 
-            pred_bbox = np.concatenate(
-                [
-                    np.reshape(pred_sbbox, (-1, 5 + num_classes)),
-                    np.reshape(pred_lbbox, (-1, 5 + num_classes)),
-                ],
-                axis=0,
-            )
+        pred_bbox = np.concatenate(
+            [np.reshape(pred_sbbox, (-1, 5 + num_classes)), np.reshape(pred_lbbox, (-1, 5 + num_classes))],
+            axis=0,
+        )
 
-            bboxes = utils.postprocess_boxes(pred_bbox, frame_size, input_size, 0.3)
-            bboxes = utils.nms(bboxes, 0.45, method="nms")
-            image = utils.draw_bbox(frame, bboxes)
+        bboxes = utils.postprocess_boxes(pred_bbox, frame_size, input_size, 0.3)
+        bboxes = utils.nms(bboxes, 0.45, method="nms")
+        image = utils.draw_bbox(frame, bboxes)
 
-            # curr_time = time.time()
-            # exec_time = curr_time - prev_time
-            result = np.asarray(image)
-            # info = "time: %.2f ms" % (1000 * exec_time)
-            if show:
-                cv2.namedWindow("result", cv2.WINDOW_NORMAL)
-                result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-                result_s = cv2.resize(result, (1920, 1080))
-                cv2.imshow("result", result_s)
-                if cv2.waitKey(1) & 0xFF == ord("q"):
-                    break
-            return result
+        result = np.asarray(image)
+        cv2.namedWindow("result", cv2.WINDOW_NORMAL)
+        result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        result_s = cv2.resize(result, (1920, 1080))
+        final = cv2.cvtColor(result_s, cv2.COLOR_RGB2BGR)
+        cv2.imshow("result", final)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
